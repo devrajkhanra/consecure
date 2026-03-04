0d8a370366492e4f0a21a65dd9add868a0ab3910
Devraj <khanradevraj@gmail.com>
2026-03-04 10:50:21 +0530

    feat: Implement ProjectService with basic CRUD operations for project management.

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -4,7 +4,6 @@ import { Repository } from 'typeorm';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { Project } from './entities/project.entity';
-import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ProjectService {
@@ -15,7 +14,6 @@ export class ProjectService {
 
   async create(createProjectDto: CreateProjectDto): Promise<Project> {
     const project = this.projectRepository.create(createProjectDto);
-    project.id = uuidv4();
     return await this.projectRepository.save(project);
   }
 
